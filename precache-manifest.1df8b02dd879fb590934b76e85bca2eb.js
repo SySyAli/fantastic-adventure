fe365181d91e0232836fd01cc3634b00196e9def
SySyAli <35876523+SySyAli@users.noreply.github.com>
2020-01-26 13:50:34 -0500

    Updates

---
+++ precache-manifest.1df8b02dd879fb590934b76e85bca2eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "288fe3238a5c16ef686f",
+    "url": "/fantastic-adventure/static/css/main.54b14c8e.chunk.css"
+  },
+  {
+    "revision": "288fe3238a5c16ef686f",
+    "url": "/fantastic-adventure/static/js/main.288fe323.chunk.js"
+  },
+  {
+    "revision": "9ee16bd076f87a9a4624",
+    "url": "/fantastic-adventure/static/css/1.2c428c5c.chunk.css"
+  },
+  {
+    "revision": "9ee16bd076f87a9a4624",
+    "url": "/fantastic-adventure/static/js/1.9ee16bd0.chunk.js"
+  },
+  {
+    "revision": "772295d01ca3a4e9b264",
+    "url": "/fantastic-adventure/static/js/runtime~main.772295d0.js"
+  },
+  {
+    "revision": "0787021fff948952f0df0646678747ea",
+    "url": "/fantastic-adventure/index.html"
+  }
+];
